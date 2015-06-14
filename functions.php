268d291986f0c3bed89054c339590673e13e5551
Leandro <lehnihon@hotmail.com>
2015-06-14 16:11:14 -0300

    retira logo wordpress

--- functions.php
+++ functions.php
@@ -1128,3 +1128,12 @@ if ( ! function_exists( 'quark_set_number_woocommerce_products' ) ) {
 	}
 	add_action( 'init', 'quark_set_number_woocommerce_products' );
 }
+
+function annointed_admin_bar_remove() {
+        global $wp_admin_bar;
+
+        /* Remove their stuff */
+        $wp_admin_bar->remove_menu('wp-logo');
+}
+
+add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
